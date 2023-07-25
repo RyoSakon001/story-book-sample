023283b28df470fa5c137b992f1358b7fcbb6b38
RyoSakon001 <ryo.development0320@gmail.com>
2023-07-25 16:11:10 +0900

    使用例

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Circle from '../stories/Circle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <h1 className='text-3xl text-red-300'>Hello</h1>
+      <Circle variant="orange" />
     </>
   )
 }
